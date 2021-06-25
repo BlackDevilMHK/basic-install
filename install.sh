1a0993cc57814d8bbdcabd9df39e6d3538eea9ed
B2A6K D3V1L-MHK <86191779+BlackDevilMHK@users.noreply.github.com>
2021-06-25 13:18:53 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,1 @@
 pkg up -y && pkg install git -y && pkg install python -y && pkg install python2 -y && pkg install openssh -y && pkg install wget -y && pkg install curl -y && pkg install nano -y && pkg install bash -y && pkg install php -y && pkg install cmatrix -y && pkg install figlet -y && pkg install ruby -y && pkg install toilet -y && pkg install proot -y && pkg install cowsay -y && pkg install tor -y && pkg install w3m -y && pkg install unzip -y && pkg install unrar -y && pkg install hydra -y && pkg install net-tools -y && pip2 install requests mechanize && gem install lolcat && pkg i perl nmap -y
-print(" ")
-print(" ")
-print(" some basic packages are installed successfully ")
-print(" thanks for using this tool ")
-print(" good bye ")
