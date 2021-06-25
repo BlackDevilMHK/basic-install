5dbc56c5cbf70122d6cdeef724cbe7d6aa7e7064
B2A6K D3V1L-MHK <86191779+BlackDevilMHK@users.noreply.github.com>
2021-06-25 10:12:10 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/bash
-
-echo ""
-
-echo ""
-
-echo ""
-
-echo -e  $g "MADE BY BlackDevilMHK
-Please  Wait  A few Minutes...... "
+echo 'MADE BY BlackDevilMHK'|lolcat
+echo 'Please  Wait  A few Minutes...... '|lolcat
 
 echo ""pkg up -y && pkg install git -y && pkg install python -y && pkg install python2 -y && pkg install openssh -y && pkg install wget -y && pkg install curl -y && pkg install nano -y && pkg install bash -y && pkg install php -y && pkg install cmatrix -y && pkg install figlet -y && pkg install ruby -y && pkg install toilet -y && pkg install proot -y && pkg install cowsay -y && pkg install tor -y && pkg install w3m -y && pkg install unzip -y && pkg install unrar -y && pkg install hydra -y && pkg install net-tools -y && pip2 install requests mechanize && gem install lolcat && pkg i perl nmap -y
 ""|lolcat
